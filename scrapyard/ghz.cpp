34ec765c74ffdd3b5b4a39c14289467594370ded
jamie <jfriel@oxfordquantumcircuits.com>
2023-07-12 12:07:57 +0000

    update example

--- scrapyard/ghz.cpp
+++ scrapyard/ghz.cpp
@@ -22,16 +22,18 @@ int main() {
 
   auto &platform = cudaq::get_platform();
   
-  // std::string backend = "oqc;qpu;/workspaces/cuda-quantum-oqc/runtime/cudaq/platform/default/rest/helpers/oqc/oqc.config";
-  // platform.setTargetBackend(backend);
+  std::string backend = "oqc;emulate;false;url;http://localhost:5000;email;{};password;{}";
+  platform.setTargetBackend(backend);
   // platform.is_simulator();
 
+  std::cout<<"creating kernel\n";
   auto kernel = cudaq::make_kernel();
   auto qubit = kernel.qalloc(2);
   kernel.h(qubit[0]);
   kernel.x<cudaq::ctrl>(qubit[0], qubit[1]);
   kernel.mz(qubit[0]);
 
+  std::cout<<"Executing\n";
   auto counts = cudaq::sample(kernel);
   counts.dump();
   
